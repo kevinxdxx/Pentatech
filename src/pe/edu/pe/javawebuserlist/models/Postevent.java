f00d8ac3f89d2aba3e46e093bdb4466979dc349d
Angel <1511687@utp.edu.pe>
2018-02-11 17:48:22 -0500

    Class Postevent

---
+++ src/pe/edu/pe/javawebuserlist/models/Postevent.java
@@ -0,0 +1,40 @@
+package pe.edu.pe.javawebuserlist.models;
+
+public class Postevent {
+    private String id;
+    private String dateIni;
+    private int quantity;
+
+    public Postevent(String id, String dateIni, int quantity) {
+        this.setId(id);
+        this.setDateIni(dateIni);
+        this.setQuantity(quantity);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Postevent setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getDateIni() {
+        return dateIni;
+    }
+
+    public Postevent setDateIni(String dateIni) {
+        this.dateIni = dateIni;
+        return this;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Postevent setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+}
