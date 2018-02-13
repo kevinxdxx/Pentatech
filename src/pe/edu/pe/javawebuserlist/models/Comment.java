a7fa8a2e44d54aa63cf9631feb3f4d0296e12a97
Maricruz <1321240@utp.edu.pe>
2018-02-13 18:34:54 -0500

    Class Comment

---
+++ src/pe/edu/pe/javawebuserlist/models/Comment.java
@@ -0,0 +1,40 @@
+package pe.edu.pe.javawebuserlist.models;
+
+public class Comment {
+    private String id;
+    private String description;
+    private String CreatedIn;
+
+    public Comment(String id, String description, String createdIn) {
+        this.id = id;
+        this.description = description;
+        CreatedIn = createdIn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Comment setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Comment setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getCreatedIn() {
+        return CreatedIn;
+    }
+
+    public Comment setCreatedIn(String createdIn) {
+        CreatedIn = createdIn;
+        return this;
+    }
+}
